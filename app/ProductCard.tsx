64ee18b81741873b24922dfe505ea34435fbb668
aaron-troglia <aaronatroglia@gmail.com>
2023-05-01 20:27:38 -0500

    fixed ProductCard context error

--- app/ProductCard.tsx
+++ app/ProductCard.tsx
@@ -4,19 +4,11 @@ import React, {useContext, useState} from 'react'
 import Link from 'next/link'
 
 export default function ProductCard({product}: {product: Product}) {
-
-    const {cart, setCart} = useContext(CartContext)
-
-    const addToCart = (product: {}) => {
-        setCart([...cart, product])
-    }
-    ``
     return (
         <div className="w-full md:w-1/3">
             <h2 className="text-xl md:text-2xl mb-2 font-bold">{product.title}</h2>
             <div>{product.price}</div>
             <Link href={`/products/${product.id}`}>View Details</Link>
-            {/* <div onClick={() => addToCart(product)}>Add to cart ({cart.length})</div> */}
         </div>
     )
 }
